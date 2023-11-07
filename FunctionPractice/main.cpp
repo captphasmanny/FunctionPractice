2e1152a948e723fb63c11d47f6e4443a00772d09
Manny <captphasmanny@att.net>
2023-11-07 07:52:08 -0500

    Initial Commit

---
+++ FunctionPractice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FunctionPractice
+//
+//  Created by Emanuel Alfaro on 11/7/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
