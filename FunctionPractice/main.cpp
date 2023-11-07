3e9fef53b13a14de9a77a9897c86764275b1610d
Manny <captphasmanny@att.net>
2023-11-07 07:56:48 -0500

    More practice with call functions into the main function.

--- FunctionPractice/main.cpp
+++ FunctionPractice/main.cpp
@@ -7,8 +7,19 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+void outA()
+{
+    std::cout << "I'm in outA!" << '\n';
+}
+
+void outB()
+{
+    std::cout << "I'm in outB!" << '\n';
+    outA();
+}
+
+int main()
+{
+    outA();
+    outB();
 }
